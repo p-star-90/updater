ee53c1b69101c074ec808ed04c873f6bd8a8d17d
p-star-90 <aurelien.clown@gmail.com>
2024-01-12 22:58:16 +0100

    Init

---
+++ mods.php
@@ -0,0 +1,21 @@
+<?php
+const EXTFILES_URL = 'http://mclife-rp.com/extfiles/mods/mods';
+const EXTFILES_FOLDER = __DIR__ . '/extfiles';
+$json = [
+    'extfiles' => []
+];
+$extfiles = [];
+$directory = array_diff(scandir(EXTFILES_FOLDER), ['.', '..']);
+foreach ($directory as $file) {
+    $extfiles[] = $file;
+}
+foreach ($extfiles as $extfile) {
+    $json['extfiles'][] = [
+        'path' => $extfile,
+        'downloadURL' => EXTFILES_URL . '/' . $extfile,
+        'sha1' => sha1_file(EXTFILES_FOLDER . '/' . $extfile),
+        'size' => filesize(EXTFILES_FOLDER . '/' . $extfile)
+    ];
+}
+header('Content-Type: application/json');
+echo json_encode($json);
