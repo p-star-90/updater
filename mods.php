9c7e61772f2dac1bc9fdba54d4fe5519037daac0
p-star-90 <78602139+p-star-90@users.noreply.github.com>
2024-01-13 18:06:06 +0100

    Add files via upload

---
+++ mods.php
@@ -0,0 +1,21 @@
+<?php
+const MODS_URL = 'https://p-star-90.github.io/updater/mods';
+const MODS_FOLDER = __DIR__ . '/mods';
+$json = [
+    'mods' => []
+];
+$mods = [];
+$directory = array_diff(scandir(MODS_FOLDER), ['.', '..']);
+foreach ($directory as $file) {
+    $mods[] = $file;
+}
+foreach ($mods as $mod) {
+    $json['extfiles'][] = [
+        'name' => $mod,
+        'downloadURL' => MODS_URL . '/' . $mod,
+        'sha1' => sha1_file(MODS_FOLDER . '/' . $mod),
+        'size' => filesize(MODS_FOLDER . '/' . $mod)
+    ];
+}
+header('Content-Type: application/json');
+echo json_encode($json);
